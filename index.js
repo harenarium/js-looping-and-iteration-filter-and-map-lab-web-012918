8112d2776988d25700a54462b303a48163c3aee2
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-11 23:59:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(function(driver){return driver.revenue > parseInt(revenue)})
+}
+
+function driverNamesWithRevenueOver(drivers, revenue){
+  return driversWithRevenueOver(drivers, revenue).map(function(driver){return driver.name})
+}
+
+function exactMatch(drivers, object){
+  return drivers.filter(function(driver){return driver[Object.keys(object)[0]] === Object.values(object)[0]})
+}
+ // - This function takes an array of driver objects as the first argument and a
+ // JavaScript object that specifies an attribute and corresponding value.
+ // For example, exactMatch(drivers, { revenue: 3000 }) will return all drivers whose revenue attribute equals 3000,
+ // and exactMatch(drivers, { name: 'Bob' }) will return all drivers whose name attribute equals Bob.
+
+function exactMatchToList(drivers, object){
+  return exactMatch(drivers, object).map(function(driver){return driver.name})
+}
+ // - This function works the same way as the exactMatch() function, except that instead of returning an array of driver objects,
+ // it returns an array of strings with each element in the array corresponding to the matching driver's name.
+ // For example, exactMatchToList(drivers, { revenue: 2000 }) will return ['Sammy'], as Sammy is the name of the matching driver object.
